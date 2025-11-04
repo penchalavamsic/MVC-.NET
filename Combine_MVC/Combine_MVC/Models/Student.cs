68b20e81a996dccf939e17328d0b83b22b8ba1e9
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-04 21:18:29 +0530

    Student class for Data storage

---
+++ Combine_MVC/Combine_MVC/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Combine_MVC.Models
+{
+    //user defined class
+    public class Student
+    {
+        public int stno { get; set; }
+        public string sname { get; set; }
+        public DateTime DOA { get; set; }
+        public int fees { get; set; }   
+    }
+}
