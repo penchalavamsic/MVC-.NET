8a14a4c7c91f5f4de29e5bcb99c67341788301bf
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-07 23:14:53 +0530

    Cricket player class

---
+++ CricketData/CricketData/Models/Cricket.cs
@@ -0,0 +1,12 @@
+﻿namespace CricketData.Models
+{
+    public class Cricket
+    {
+        //player details
+        public int id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public int runs { get; set; }
+        public DateTime GameDay { get; set; }
+    }
+}
