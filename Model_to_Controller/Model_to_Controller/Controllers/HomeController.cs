e78da425a860c88a0d6f960d7562efa59b6ba026
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-11 21:08:07 +0530

    Model to controller & View

---
+++ Model_to_Controller/Model_to_Controller/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Model_to_Controller.Models;
+
+namespace Model_to_Controller.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Address(Address obj) //model binding
+        {
+            return View(obj);
+        }
+    }
+}
---
+++ Model_to_Controller/Model_to_Controller/Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Model_to_Controller.Models
+{
+    public class Address
+    {
+        public string DoorNo { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
